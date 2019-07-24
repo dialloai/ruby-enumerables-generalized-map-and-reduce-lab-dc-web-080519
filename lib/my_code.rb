655ee5b440f634070b9ac31e56d576ba50d192de
Aicha D <aicha1diallo@gmail.com>
2019-07-24 21:56:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,17 @@
-# Your Code Here
+def map (array)
+ results= []
+ array.each do |number|
+  # binding.pry
+    results.push (yield number)
+  end
+  return results
+end
+
+def reduce (array)
+  value =0 
+ array.each do |number|
+  # binding.pry
+ yield(value, number)
+  end
+  return value 
+end
